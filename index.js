f97e543f58a5bac17dca5fb765975732292853d1
kahoragachau <joelkahoragachau6@gmail.com>
2022-08-05 18:27:18 +0300

    -Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector('main').remove();
+const newHeader = document.createElement("h1");
+const textNode = document.createTextNode("YOUR-NAME is the champion");
+newHeader.setAttribute("id","victory");
+newHeader.appendChild(textNode);
